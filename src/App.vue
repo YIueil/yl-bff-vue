7110643b52fc07c7495f75ceb0d2616917bd89f2
YIueil <YIueil@163.com>
2022-11-23 22:15:32 +0800

    引入 iconfont 图标库

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <span class="iconfont">&#xe66b;</span>
+    <span class="iconfont yl-icon-image"></span>
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#yl-icon-image"></use>
+    </svg>
     <nav>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
@@ -26,5 +31,13 @@ html body {
   margin: 0;
   padding: 0;
   border: 0;
+}
+
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/iconfont/iconfont.css'
+import '@/assets/iconfont/iconfont' // iconfont 图标库
 
 import './core/directives/index'
 
