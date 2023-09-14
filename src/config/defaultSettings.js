ca31b6f97e8b7231e11992e287a215ab61a8051a
YIueil <YIueil@163.com>
2023-09-14 18:07:27 +0800

    feat: 添加默认配置

---
+++ src/config/defaultSettings.js
@@ -0,0 +1,11 @@
+/**
+ * Author: YIueil
+ * Date: 2023/9/14 17:45
+ * Description: 默认设置
+ */
+export default {
+  navTheme: 'light',
+  primaryColor: '#1890ff',
+  title: 'yl-acc',
+  production: process.env.NODE_ENV === 'production'
+}
