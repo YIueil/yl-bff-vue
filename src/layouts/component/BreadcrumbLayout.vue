2c5c0cc44159a3937db3a8d1c750cf848e40209b
YIueil <YIueil@163.com>
2023-09-21 00:01:15 +0800

    feat: 面包屑layout

---
+++ src/layouts/component/BreadcrumbLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <a-breadcrumb separator="/">
+      <a-breadcrumb-item v-for="item in breadcrumb" :key="item.title" href="">
+        <a :href="item.path" @click.prevent="$router.push(item.path)">
+          <a-icon :type="item.icon"/>
+          <span>{{ item.title }}</span>
+        </a>
+      </a-breadcrumb-item>
+    </a-breadcrumb>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BreadcrumbLayout',
+  components: {},
+  props: {},
+  data() {
+    return {
+      breadcrumb: this.$route.meta.breadcrumb
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  mounted() {}
+}
+</script>
+
+<style scoped>
+
+</style>
