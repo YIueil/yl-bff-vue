3037c4f326a98283b321d2633ebf7d1dfc143819
YIueil <YIueil@163.com>
2023-09-27 18:31:00 +0800

    fix: 生命周期去除刷新监听

---
+++ src/config/lifecycle-hooks.js
@@ -0,0 +1,18 @@
+/**
+ * Author: YIueil
+ * Date: 2023/9/26 10:47
+ * Description: 生命周期钩子 提供重要钩子的回调
+ */
+
+/**
+ * 登录成功回调
+ */
+export function onLoginSuccess() {
+  console.log('onLoginSuccess')
+}
+/**
+ * 退出登录回调
+ */
+export function onLogoutSuccess() {
+  console.log('onLogoutSuccess')
+}
